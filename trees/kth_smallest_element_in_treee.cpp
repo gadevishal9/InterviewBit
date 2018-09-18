1016e581170a28c33f04de6f281d5727fc55a7dc
vishal gade <vishalgade95@vishals-mbp.3520.dhcp.asu.edu>
2018-09-18 15:54:59 -0700

    added new files

--- trees/kth_smallest_element_in_treee.cpp
+++ trees/kth_smallest_element_in_treee.cpp
@@ -2,3 +2,41 @@
 // Created by vishal gade on 9/18/18.
 //
 
+/**
+ * Definition for binary tree
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+void nodes(TreeNode *root,int &B,int &value)
+{
+    if(root == NULL) return;
+    nodes(root->left,B,value);
+
+    if(B > 1) B = B-1;
+    else if (B == 1 ){
+        //cout<<"i am in";
+        value  = root->val;
+        B = B -1;
+        return;
+
+    }
+    else return;
+
+    nodes(root->right,B,value);
+
+}
+int Solution::kthsmallest(TreeNode* A, int B) {
+    int value =-10;
+    nodes(A,B,value);
+    return value;
+
+
+
+
+
+
+}
