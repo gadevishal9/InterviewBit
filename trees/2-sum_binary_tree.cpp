565a4cad39b39f83a0a3eb4329f548540eb3fc65
vishal gade <vishalgade95@vishals-mbp.3520.dhcp.asu.edu>
2018-09-18 15:57:17 -0700

    added new files

--- trees/2-sum_binary_tree.cpp
+++ trees/2-sum_binary_tree.cpp
@@ -2,3 +2,30 @@
 // Created by vishal gade on 9/18/18.
 //
 
+/**
+ * Definition for binary tree
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+int sum(TreeNode* root,int B,unordered_map<int,int> &hash)
+{
+    if(root == NULL) return 0;
+    if(hash[B-root->val] == 1)
+    {
+        return 1;
+    }
+    else hash[root->val] =1;
+
+    return sum(root->left,B,hash)+sum(root->right,B,hash);
+}
+
+int Solution::t2Sum(TreeNode* A, int B) {
+    unordered_map<int,int> hash;
+    //cout<<sum(A,B,hash);
+    return (sum(A,B,hash));
+
+}
