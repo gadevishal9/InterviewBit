b208f7ede016e9f870b8c0af5b5339c03c87bd27
vishal gade <vishalgade95@vishals-mbp.3520.dhcp.asu.edu>
2018-09-18 15:59:39 -0700

    added new files

--- trees/invert_binary_tree.cpp
+++ trees/invert_binary_tree.cpp
@@ -2,3 +2,27 @@
 // Created by vishal gade on 9/18/18.
 //
 
+/**
+ * Definition for binary tree
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+void invert(TreeNode *root)
+{
+
+}
+TreeNode* Solution::invertTree(TreeNode* A) {
+    if(A == NULL) return A;
+    if(A->left != NULL) invertTree(A->left);
+    {
+        TreeNode *temp = A->left;
+        A->left = A->right;
+        A->right = temp;
+    }
+    if(A->left != NULL) invertTree(A->left);
+    return A;
+}
