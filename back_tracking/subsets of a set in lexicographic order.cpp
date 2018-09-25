77646ea37afdc5fe45a0986bd556ee8e10776f4f
vishal gade <vishalgade95@vishals-mbp.3520.dhcp.asu.edu>
2018-09-24 20:35:11 -0700

    added new program in back_tracking

--- back_tracking/subsets of a set in lexicographic order.cpp
+++ back_tracking/subsets of a set in lexicographic order.cpp
@@ -2,3 +2,24 @@
 // Created by vishal gade on 9/24/18.
 //
 
+void subset(vector<int> &A,vector<vector<int>> &ans,vector<int> curr,int j)
+{
+
+    ans.push_back(curr);
+    for(int i=j;i<A.size();i++)
+    {
+        curr.push_back(A[i]);
+        subset(A,ans,curr,i+1);
+        curr.pop_back();
+    }
+
+}
+
+vector<vector<int> > Solution::subsets(vector<int> &A) {
+    vector<vector<int>> ans;
+    vector<int> curr;
+    sort(A.begin(),A.end());
+    subset(A,ans,curr,0);
+    sort(ans.begin(),ans.end());
+    return ans;
+}
